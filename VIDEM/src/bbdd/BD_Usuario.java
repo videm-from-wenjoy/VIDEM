c15c737d74f6004b1d3adc6f78130cd56dc8d2c3
admin <admin@Win7_1TB>
2019-04-29 09:31:21 +0200

    Creacion de dos métodos: editar usuario y listar usuarios.

--- VIDEM/src/bbdd/BD_Usuario.java
+++ VIDEM/src/bbdd/BD_Usuario.java
@@ -9,6 +9,8 @@ import java.sql.*;
 import modelos.Cliente;
 import modelos.Empleado;
 import modelos.Usuario;
+import java.util.*;
+
 public class BD_Usuario extends BD_Conector {
 	private static Statement s;		
 	private static ResultSet reg;
@@ -16,7 +18,12 @@ public class BD_Usuario extends BD_Conector {
 	public BD_Usuario(String bbdd){
 		super (bbdd);
 	}
-	
+	/**
+	 * 
+	 * @param 
+	 * @return Devuelve verdadero(true) si se ha podido añadir un nuevo usuario y falso(false)
+	 * si no se puede añadir un usuario
+	 */
 	public boolean añadir_Usuario(Usuario user) {
 		String cadenaSQL="INSERT INTO usuarios VALUES('"+user.getEmail()+"','"+user.getPassword()+"','"
 						+user.getNombre()+"','"+user.getDomicilio()+"','"+user.getDni()+"','"+user.getRol()+"','"
@@ -34,6 +41,12 @@ public class BD_Usuario extends BD_Conector {
 		}
 		
 	}
+	/**
+	 * 
+	 * @param user
+	 * @param num
+	 * @return 
+	 */
 	public boolean borrar_Usuario(Usuario user,int num) {
 		String cadenaSQL="DELETE FROM usuarios WHERE EMAIL='"+user.getEmail()+"'";
 		String cadena2="";
@@ -77,6 +90,11 @@ public class BD_Usuario extends BD_Conector {
 			return false;
 		}
 	}
+	/**
+	 * 
+	 * @param user
+	 * @return
+	 */
 	
 	public String login(Usuario user){
 		String cadena="SELECT ROL FROM usuarios WHERE EMAIL='" + user.getEmail() +"' AND PASSWORD='" + user.getPassword() +"'";
@@ -97,7 +115,11 @@ public class BD_Usuario extends BD_Conector {
 			
 		}
 	}
-	
+	/**
+	 * 
+	 * @param user
+	 * @return
+	 */
 	public String loginEncargado(Usuario user){
 		String cadena="SELECT PUESTO FROM empleados WHERE PUESTO IN ('ENCARGADO','ADMINISTRADOR')";
 		try{
@@ -117,4 +139,61 @@ public class BD_Usuario extends BD_Conector {
 			
 		}
 	}
+	/**
+	 * 
+	 * @param user
+	 * @param campo
+	 * @param contenido
+	 * @return
+	 */
+	public int editarUsuario(Usuario user, int campo, String contenido) {
+		String cadenaSQL="";
+		if (campo==1) 
+			cadenaSQL="UPDATE usuarios SET EMAIL='"+contenido+"' WHERE DNI='"+user.getDni()+"'";
+		if (campo==2)
+			cadenaSQL="UPDATE usuarios SET PASSWORD='"+contenido+"' WHERE DNI='"+user.getDni()+"'";
+		if(campo==3)
+			cadenaSQL="UPDATE usuarios SET ROL='"+contenido+"' WHERE DNI='"+user.getDni()+"'";
+		if(campo==4)
+			cadenaSQL="UPDATE usuarios SET NOMBRE='"+contenido+"' WHERE DNI='"+user.getDni()+"'";
+		if(campo==5)
+			cadenaSQL="UPDATE usuarios SET DIRECCION='"+contenido+"' WHERE DNI='"+user.getDni()+"'";
+		if(campo==6)
+			cadenaSQL="UPDATE usuarios SET TELEFONO='"+contenido+"' WHERE DNI='"+user.getDni()+"'";
+		try {
+			this.abrir();
+			s = c.createStatement();
+			int filas = s.executeUpdate(cadenaSQL);
+			s.close();
+			this.cerrar();
+			return filas;
+		}catch(SQLException e) {
+			this.cerrar();
+			return -1;
+		}
+	}
+	/**
+	 * Muestra una lista completa de todos los usuarios.
+	 * @return
+	 */
+	public Vector<Usuario> listarUsuarios(){
+		String cadenaSQL="SELECT * FROM usuarios";
+		Vector<Usuario> users = new Vector<Usuario>();
+		try {
+			this.abrir();
+			s = c.createStatement();
+			reg = s.executeQuery(cadenaSQL);
+			while(reg.next()) {
+				users.add(new Usuario(reg.getString("EMAIL"),reg.getString("PASSWORD"),reg.getString("ROL"),reg.getString("NOMBRE"),reg.getString("DNI"),reg.getString("DIRECCION"),reg.getInt("TELEFONO")));
+			}
+			s.close();
+			this.cerrar();
+			return users;
+		}catch(SQLException e) {
+			this.cerrar();
+			return null;
+		}
+	}
+	
+	
 }
