27c9a8867c9a4b5425f46685fba7935284819fc2
admin <admin@Win7_1TB>
2019-04-29 09:28:05 +0200

    Main Actualizado: Falta completar partes de las funciones de los empleados y toda la parte de los clientes.

--- VIDEM/src/principal/Main.java
+++ VIDEM/src/principal/Main.java
@@ -1,11 +1,14 @@
 package principal;
 import java.util.*;
-
 import bbdd.BD_Usuario;
 import bbdd.BD_Videojuego;
 import modelos.Usuario;
 import modelos.Videojuego;
-
+/**
+ * 
+ * @author Gonzalo Fernández
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
@@ -25,18 +28,48 @@ public class Main {
 		user = new Usuario(email,clave);				
 		String opc=bbdd.login(user);
 		
-		if(opc=="EMPLEADO") {
+		if(opc.equalsIgnoreCase("EMPLEADO") && opc.equalsIgnoreCase("CLIENTE") || opc==null) {
+			System.out.println("Error, no estas en la base de datos");
+			System.out.println("¿Quieres registrarte en VIDEM? S/N");
+			char respuesta=sc.nextLine().charAt(0);
+			
+			if(respuesta=='S') {
+				System.out.println("Correo electronico");
+				email=sc.nextLine();
+				System.out.println("Clave personal:");
+				clave=sc.nextLine();
+				System.out.println("Nombre:");
+				String nombre=sc.nextLine();
+				System.out.println("Dni:");
+				String dni=sc.nextLine();
+				System.out.println("Domicilio:");
+				String domicilio=sc.nextLine();
+				System.out.println("Telefono:");
+				int telefono=sc.nextInt();
+				user = new Usuario(email,clave,nombre,domicilio,dni,"CLIENTE",telefono);
+				if ( bbdd.añadir_Usuario(user))
+					System.out.println("Se ha dado de alta en VIDEM");
+				else
+					System.out.println("No se ha podido dar de alta en VIDEM, por favor intentelo más tarde.");
+			}
+			else {
+				System.out.println("Vuelva pronto");
+			}
+		}
+		if(opc.equalsIgnoreCase("EMPLEADO")) {
 			String opc2=bbdd.loginEncargado(user);
 			if(opc2=="ADMINISTRADOR") {
 				do {
 					System.out.println("1º Ver solicitudes");
+					System.out.println("2º ");
+					opc3=sc.nextInt();
 				}while(opc3!= 4);
 			}
-			if(opc2=="ENCARGADO") {
+			if(opc2.equalsIgnoreCase("ENCARGADO")) {
 				do {
 					System.out.println("1º Añadir videojuego");
 					System.out.println("2º Borrar videojuego");
-					System.out.println("3º ");
+					System.out.println("3º Recuento de ventas");
 					opc3=sc.nextInt();
 				}while(opc3!= 4);
 			}
@@ -44,7 +77,7 @@ public class Main {
 				
 			}
 		}
-		else {
+		if(opc.equalsIgnoreCase("CLIENTE")) {
 			
 		}
 	}
